23695ee480e7ff9132ac47db6a4b4b7278d97ff5
Jane Justiz <janejustiz@gmail.com>
2018-05-19 15:44:53 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -6,14 +6,12 @@ def turn_count(board)
     if input == "X" || input == "O"
       counter += 1
     end}
-  puts counter
 end
 
 turn_count(board)
 
 def current_player(board)
   counter = turn_count(board)
-  puts counter
   if counter % 2 == 0
     "O"
   else
