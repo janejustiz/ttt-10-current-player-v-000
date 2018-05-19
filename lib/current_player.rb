4e4120b7b8eff639fa28fde9389c5e45912093bc
Jane Justiz <janejustiz@gmail.com>
2018-05-19 14:25:10 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -11,7 +11,7 @@ def turn_count(board)
 end
 
 
-def current_player
+def current_player(board)
   turn_count(board)
   if counter % 2 == 0
     "O"
@@ -21,4 +21,4 @@ def current_player
 end
 
 
-current_player
+current_player(board)
