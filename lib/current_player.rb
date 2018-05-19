af07e26e3fe924cfce6d34e398157d6bdaa328a3
Jane Justiz <janejustiz@gmail.com>
2018-05-19 14:28:19 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,5 @@
 board=[" "," "," "," "," "," "," "," "," "]
 
-
 def turn_count(board)
   counter = 0
   if board.each == "X" || board.each == "O"
@@ -8,15 +7,13 @@ def turn_count(board)
   end
 end
 
-
 def current_player(board)
   counter = turn_count(board)
-  if counter % 2 == 0
+  if counter%2 == 0
     "O"
   else
     "X"
   end
 end
-
 
 current_player(board)
