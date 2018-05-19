c95af833b8fa08b871d8e05acab76de483ff1d1f
Jane Justiz <janejustiz@gmail.com>
2018-05-19 15:42:34 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,10 +2,10 @@ board=["X", " "]
 
 def turn_count(board)
   counter = 0
-  board.each {|input|
+  board.each |input|
     if input == "X" || input == "O"
       counter += 1
-    end}
+    end
   puts counter
 end
 
